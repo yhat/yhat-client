f8ecb55e23939196de6049e0cc499fcb563128e3
Greg Lamp <glamp@Gregs-MacBook-Air.local>
2012-11-19 20:31:48 -0500

    initial commit

---
+++ skdeploy/api.py
@@ -0,0 +1,75 @@
+import requests
+import json
+import pickle
+import inspect
+
+BASE_URI = "http://ec2-54-242-232-2.compute-1.amazonaws.com:5000/"
+
+class API(object):
+    def __init__(self, base_uri):
+        self.base_uri = base_uri
+
+    def get(self, endpoint, params):
+        try:
+            return requests.get(self.base_uri + endpoint + "?", params=params).json
+        except Exception, e:
+            raise e
+    
+    def post(self, endpoint, params, data):
+        try:
+            return requests.post(self.base_uri + endpoint + "?",
+                                 params=params,
+                                 data=json.dumps(data)).json
+        except Exception, e:
+            raise e
+
+class Skdeploy(API):
+    def __init__(self, username, apikey):
+        self.username = username
+        self.apikey = apikey
+        self.base_uri = BASE_URI
+        self.q = {"username": self.username, "apikey": apikey}
+
+    def show_models(self):
+        return self.get("showmodels", self.q)
+
+    def raw_predict(self, model, data):
+        data = {"data": data}
+        q = self.q
+        q['model'] = model
+        return self.post('predict', q, data)
+
+    def predict(self, model, data):
+        rawResponse = self.raw_predict(model, data)
+        return rawResponse['prediction']
+
+    def upload(self, modelname, pml):
+        className = pml.__class__.__name__
+        filesource = "\n"
+        filesource += "class %s(PML):" % className + "\n"
+        filesource += inspect.getsource(pml.transform)+ "\n"
+        filesource += inspect.getsource(pml.predict)
+
+        userFiles = vars(pml)
+        pickledUserFiles = {}
+        for f, uf in userFiles.iteritems():
+            pickledUserFiles[f] = pickle.dumps(uf)
+        payload = {
+            "modelname": modelname,
+            "modelfiles": pickledUserFiles,
+            "code": filesource,
+            "className": className
+        }
+
+        return self.post("model", self.q, payload)
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,64 @@
+from skdeploy import Skdeploy
+import pprint as pp
+import requests
+import json
+import pickle
+from skdeploy.pml import PML
+
+
+
+raw_data = [[  0.,   0.,   5.,  13.,   9.,   1.,   0.,   0.],
+           [  0.,   0.,  13.,  15.,  10.,  15.,   5.,   0.],
+           [  0.,   3.,  15.,   2.,   0.,  11.,   8.,   0.],
+           [  0.,   4.,  12.,   0.,   0.,   8.,   8.,   0.],
+           [  0.,   5.,   8.,   0.,   0.,   9.,   8.,   0.],
+           [  0.,   4.,  11.,   0.,   1.,  12.,   7.,   0.],
+           [  0.,   2.,  14.,   5.,  10.,  12.,   0.,   0.],
+           [  0.,   0.,   6.,  13.,  10.,   0.,   0.,   0.]]
+
+skd = Skdeploy("glamp", "abcd1234")
+
+pp.pprint(skd.show_models())
+print "*"*80
+pp.pprint(skd.raw_predict('irisTree', [2, 3, 2, 2]))
+print "*"*80
+pp.pprint(skd.predict('digits', raw_data))
+print "*"*80
+
+
+class DecisionTreePML(PML):
+    def transform(self, rawData):
+        pair = [2, 3]
+        data = np.array(rawData)
+        X = data[:, pair]
+        mean = self.xtrain.mean(axis=0)
+        std = self.xtrain.std(axis=0)
+        X = (X - mean) / std
+        return X
+
+    def predict(self, data):
+        return self.clf.predict(data)
+
+
+clf = pickle.load(open('./skdeploy/test/decisiontree.model', 'rb'))
+xtrain = pickle.load(open('./skdeploy/test/xtrain.model', 'rb'))
+
+myTree = DecisionTreePML(clf=clf, xtrain=xtrain)
+
+
+
+pp.pprint(skd.upload("gregsTree", myTree))
+
+
+
+
+
+
+
+
+
+
+
+
+
+

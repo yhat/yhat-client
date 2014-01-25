77fa3470034bce5d8acb95027a4a126f59e23b26
Greg <greg@yhatHQ.com>
2014-01-25 13:38:46 -0500

    spacing

--- yhatcli/credentials.py
+++ yhatcli/credentials.py
@@ -4,25 +4,26 @@ import os
 
 
 def setup():
-	"""
-	Prompts the user for their credentials and the saves them to a Yhat "dot" file.
-	"""
-	username = raw_input("Yhat username: ")
-	apikey = raw_input("Yhat apikey: ")
-	with open(os.path.join(os.environ['HOME'], '.yhat', '.config'), 'w') as f:
+    """
+    Prompts the user for their credentials and the saves them to a Yhat "dot" file.
+    """
+    username = raw_input("Yhat username: ")
+    apikey = raw_input("Yhat apikey: ")
+    with open(os.path.join(os.environ['HOME'], '.yhat', '.config'), 'w') as f:
 		data = json.dumps({"username": username, "apikey": apikey})
 		data = base64.encodestring(data)
 		f.write(data)
 
 def read():
-	"""
-	Extracts credentials from a "dot" file
-
-	Returns
-	=======
-	credentials: dict; your credentials in form: {"username": "YOUR USERNAME", 
-	"apikey": "YOUR APKIKEY}"
-	"""
-	data = open(os.path.join(os.environ['HOME'], '.yhat', '.config')).read()
-	return json.loads(base64.decodestring(data))
+    """
+    Extracts credentials from a "dot" file
+    
+    Returns
+    =======
+    credentials: dict
+        your credentials in form:
+        {"username": "YOUR USERNAME", "apikey": "YOUR APKIKEY}"
+    """
+    data = open(os.path.join(os.environ['HOME'], '.yhat', '.config')).read()
+    return json.loads(base64.decodestring(data))
 

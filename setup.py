753e880a9cc71fd6a6ad577bda9dfcb75d780d87
Greg <greg@yhatHQ.com>
2014-01-27 14:11:47 -0500

    switching out name

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-        name="yhatcli",
+        name="yhat",
         version="0.6.0",
         author="Greg Lamp",
         author_email="greg@yhathq.com",
-        url="https://github.com/yhat/cli",
- #       license=open("LICENSE.txt").read(),
+        url="https://github.com/yhat/yhat-client",
         packages=find_packages(),
         description="plotting in the terminal",
-#        long_description=open("README.txt").read(),
         install_requires=[],
         scripts=['bin/yhat-cli']
 )

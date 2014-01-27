4ffe3cf318f88f32046876074862355ff63f962b
Greg <greg@yhatHQ.com>
2014-01-27 16:25:57 -0500

    prepping for release

--- setup.py
+++ setup.py
@@ -2,13 +2,29 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-        name="yhat",
-        version="0.6.0",
-        author="Greg Lamp",
-        author_email="greg@yhathq.com",
-        url="https://github.com/yhat/yhat-client",
-        packages=find_packages(),
-        description="plotting in the terminal",
-        install_requires=[],
-        scripts=['bin/yhat-cli']
+    name="yhat",
+    version="0.6.0",
+    author="Greg Lamp",
+    author_email="greg@yhathq.com",
+    url="https://github.com/yhat/yhat-client",
+    packages=find_packages(),
+    description="Python client for Yhat (http://yhathq.com/)",
+    license="BSD",
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+    install_requires=[
+        "colorama==0.2.5",
+        "progressbar==2.3",
+        "Flask==0.10.1",
+        "websocket-client==0.12.0",
+        "prettytable==0.7.2"
+    ],
+    keywords=['yhat', 'scikits', 'numpy', 'pandas'],
+    scripts=['bin/yhat-cli']
 )

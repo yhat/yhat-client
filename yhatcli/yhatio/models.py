95e3c7ee3c353b1982f4107771d6b46eb1928a8b
Greg <greg@yhatHQ.com>
2014-01-27 13:57:33 -0500

    adding in step model

--- yhatcli/yhatio/models.py
+++ yhatcli/yhatio/models.py
@@ -1,5 +1,8 @@
 import sys
 import json
+import inspect
+import pickle
+
 from flask import Flask, request, render_template, jsonify
 import pandas as pd
 
@@ -18,7 +21,20 @@ class YhatModel(object):
     The `run` method provides you with a basic development "server" for testing
     locally. It will give you the same results as you will see on a Yhat server
     99% of the time.
+
+    Parameters
+    ----------
+    requirements: string
+        path to requirements.txt
+    filename: string
+        filename of saved model
     """
+    def __init__(self, requirements="", filename=None):
+        if filename is not None:
+            pickles = json.load(open(filename, 'rb'))
+            for varname, pickled_value in pickles.get('objects', {}).items():
+                globals()[varname] = pickle.loads(pickled_value)
+        self.requirements = requirements
 
     def execution_plan(self, data):
         return self.execute(data)
@@ -141,3 +157,60 @@ class BaseModel(Model):
         self.require()
         data = self.transform(data)
         return self.predict(data)
+
+
+class StepModel(Model):
+    __name__ = "StepModel"
+    """
+    StepModel allows you to define steps as methods. Each step is numbered
+    (i.e. step_1, step_2, step_3, etc.). Steps are executed in order with the 
+    output from the previous step becoming the input to the next step.
+
+    Parameters
+    ----------
+    
+    Examples
+    --------
+
+    class MyModel(StepModel):
+        def step_1(self, data):
+            return 1
+        def step_2(self, data):
+            return [data, 2]
+        def step_3(self, data):
+            return data + [3]
+    mm = MyModel()
+    print mm.execute(None)
+    # [1, 2, 3]
+    # This executes like so...
+    # one = mm.step_1(None)
+    # two = mm.step_2(one)
+    # three = mm.step_3(two)
+    # print three
+    """
+    def require(self):
+        pass
+
+    def _get_steps(self):
+        steps = []
+        for name, step in inspect.getmembers(self, predicate=inspect.ismethod):
+            if re.match("step_", name):
+                order = name.lstrip("step_")
+                try:
+                    order = int(order)
+                except:
+                    raise Exception("'%s' - step names must be integers" % name)
+                steps.append((order, name, step))
+        steps = sorted(steps, key=lambda x: int(x[0]))
+        return steps
+    
+    def execute(self, data):
+        for _, name, step in self._get_steps():
+            try:
+                data = step(data)
+            except Exception, e:
+                print "Error executing step: '%s'" % name
+                print "Exception: %s" % str(e)
+                return
+        return data
+

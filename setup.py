d609198b8a0740f24f77209968f2c18a76f4ddf1
Greg <greg@yhatHQ.com>
2014-01-28 22:19:55 -0500

    scripting release

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "websocket-client==0.12.0",
         "prettytable==0.7.2"
     ],
-    # pandoc -f markdown -t rst README.md > README.rst
     long_description=open("README.rst").read(),
     keywords=['yhat', 'scikits', 'numpy', 'pandas'],
     scripts=['bin/yhat-cli']

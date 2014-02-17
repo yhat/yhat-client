f3187fd723bb75e8915aafcd16c1ca2f36ee504c
Greg <greg@yhatHQ.com>
2014-02-17 09:16:56 -0500

    inc version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="yhat",
-    version="0.6.6",
+    version="0.6.7",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/yhat-client",

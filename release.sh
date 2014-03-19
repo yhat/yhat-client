e02c02a202e16cff0419a8840840b41fc2656251
Greg <greg@yhatHQ.com>
2014-03-19 13:16:40 -0500

    bump for release

--- release.sh
+++ release.sh
@@ -3,8 +3,11 @@
 pandoc -f markdown -t rst README.md > README.rst
 python setup.py install
 v1=`pip freeze | grep "yhat=="`
-v2=`python -c "import yhat; print yhat.__version__"`
-if [[ "${v1}"!="${v2}" ]]; then
+v2=$(echo "yhat=="`python -c "import yhat; print yhat.__version__"`"")
+if [[ "${v1}" != "${v2}" ]]; then
+  echo "${v1} vs ${v2}"
+  exit 1
+else
+  python setup.py install sdist $1
 fi
-python setup.py install sdist $1
 

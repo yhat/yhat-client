d3f45fa87c31960d9a5ae8867937048cab25747f
ericchiang <eric.chiang.m@gmail.com>
2014-03-25 18:01:49 -0400

    df_to_json() function added

--- yhat/__init__.py
+++ yhat/__init__.py
@@ -4,6 +4,7 @@ from .deployment.models import StepModel
 from .deployment.models import YhatModel
 # preprocessing decorator
 from .deployment.input_and_output import preprocess
+from .deployment.input_and_output import df_to_json
 # client
 from .api import Yhat
 

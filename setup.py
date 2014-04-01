b4ff708892a90b5890126f0374131fc348a67ed6
ericchiang <eric.chiang.m@gmail.com>
2014-04-01 15:30:26 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="yhat",
-    version="0.6.17",
+    version="0.6.18",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/yhat-client",

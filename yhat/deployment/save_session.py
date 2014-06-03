f4fcfc8da34692468d286daeee7ac27937cf9918
Greg Lamp <lamp.greg@gmail.com>
2014-06-03 12:35:02 -0400

    trycatch around terragon

--- yhat/deployment/save_session.py
+++ yhat/deployment/save_session.py
@@ -266,7 +266,7 @@ def save_function(function, session):
     imports = sorted(list(set(imports)))
     imports.append("import json")
     imports.append("import pickle")
-    imports.append("from yhat.deployment import terragon")
+    imports.append("try:\n    from yhat.deployment import terragon\n    except:\n    pass")
     source_code = "\n".join(imports) + "\n\n\n" + source_code
     pickles = {
         "objects": pickles,

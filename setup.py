2f45bf785a078ad02e92aa218927a753be4caa50
glamp <lamp.greg@gmail.com>
2015-09-30 10:55:47 -0400

    bump for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="yhat",
-    version="1.3.7",
+    version="1.3.8",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/yhat-client",

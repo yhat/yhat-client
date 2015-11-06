dd1dbe65874e79133e976d8d21e774bd4fd2587f
Eric Chiang <eric.chiang.m@gmail.com>
2015-11-06 09:00:24 -0500

    v1.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="yhat",
-    version="1.3.8",
+    version="1.4.0",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/yhat-client",

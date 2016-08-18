333283ceca508475ecf8db421a4324ecb30d26c0
Greg Lamp <lamp.greg@gmail.com>
2016-08-18 15:47:04 -0400

    Docs for Variant

--- yhat/__init__.py
+++ yhat/__init__.py
@@ -1,7 +1,7 @@
 # models
 from .deployment.models import BaseModel
 from .deployment.models import YhatModel
-from .deployment.models import SplitTestModel
+from .deployment.models import SplitTestModel, Variant
 # preprocessing decorator
 from .deployment.input_and_output import preprocess
 from .deployment.input_and_output import df_to_json
--- yhat/deployment/models.py
+++ yhat/deployment/models.py
@@ -48,25 +48,76 @@ class YhatModel(object):
         """
         pass
 
+class Variant(dict):
+    """
+    A Variant is an option to use in a SplitTestModel. Variants are composed of 3 properties:
+
+        - label: string, this will be included in the model output to indicate which variant was run in the split test
+        - method: string, this is a reference to the method that will be used when this variant is used
+        - traffic_allocation: float, the percentage of traffic that will be routed to this variant for the split test
+
+    >>> variants = [
+    ...    Variant("A", "execute_a", 0.5),
+    ...    Variant("B", "execute_b", 0.5),
+    ... ]
+    >>> variants = [
+    ...    Variant("A", "execute_a", 0.5),
+    ...    Variant("B", "execute_b", 0.3),
+    ...    Variant("C", "execute_c", 0.2),
+    ... ]
+    >>> variants = [
+    ...    Variant("SVM", "execute_svc", 0.7),
+    ...    Variant("Logistic", "execute_logit", 0.3)
+    ... ]
+    """
+    def __init__(self, label=None, method=None, traffic_allocation=0.5):
+        self['label'] = label
+        self['method'] = method
+        self['traffic_allocation'] = traffic_allocation
+
+
 class SplitTestModel(YhatModel):
     """
     Create an A/B testable model. This will split traffic between 2 different
     `execute` methods.
+
+    [
+        { "label": "A", "method": execute_a, "traffic_allocation": 0.5 },
+        { "label": "B", "method": execute_b, "traffic_allocation": 0.4 },
+        { "label": "C", "method": execute_c, "traffic_allocation": 0.1 }
+    ]
+
     """
 
-    def execute(self, data):
-        if random.random() > 0.5:
-            data['variant'] = "A"
-            return self.execute_a(data)
-        else:
-            data['variant'] = "B"
-            return self.execute_b(data)
+    def __init__(self, variants=None):
+        if variants is None:
+            variants = self.variants
 
-    def execute_a(self, data):
-        pass
-    
-    def execute_b(self, data):
-        pass
+        if sum(v['traffic_allocation'] for v in variants) != 1:
+            msg = " + ".join([str(v['traffic_allocation']) for v in variants])
+            msg += " = " + str(sum(v['traffic_allocation'] for v in variants))
+            raise Exception("Your total traffic allocation for all variants must sum to 1!\n\t%s" % msg)
+
+        for variant in variants:
+            try:
+                getattr(self, variant['method'])
+            except:
+                raise Exception("Method '{}' for variant '{}' does not exist".format(variant['method'], variant['label']))
+
+        self.variants = variants
+        super(SplitTestModel, self).__init__()
+
+
+    def execute(self, data):
+        r = random.random()
+        total = 0.0
+        for variant in self.variants:
+            total += variant['traffic_allocation']
+            if r < total:
+                method = getattr(self, variant['method'])
+                output = method(data)
+                output['variant'] = variant['label']
+                return output
 
 class Model(object):
     def __init__(self, **kwargs):

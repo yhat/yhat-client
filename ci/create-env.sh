0f1b9257ad2cefa2bad0032f68ca77b409cbc69c
Greg Lamp <lamp.greg@gmail.com>
2016-10-24 17:49:54 -0400

    install python27 and python35

---
+++ ci/create-env.sh
@@ -0,0 +1,10 @@
+# Configure the conda environment and put it in the path using the
+# provided versions
+
+PYTHON_VERSION="$1"
+
+ENVNAME="test-env-${PYTHON_VERSION}"
+
+REQUIREMENTS="python${PYTHON_VERSION} numpy"
+conda create -n "${ENVNAME}" --quiet --yes $REQUIREMENTS
+source activate "${ENVNAME}"

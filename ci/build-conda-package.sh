c7c2bc1439673d5e50da426c984fe9bc2ac44b62
Greg Lamp <lamp.greg@gmail.com>
2016-10-24 18:47:30 -0400

    committing changes

--- ci/build-conda-package.sh
+++ ci/build-conda-package.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-bucket=$1
-
-if [ "$bucket" == "" ]; then
-    echo "usage: build [bucket]"
-    exit 2
-fi
-
+bucket="yhat-conda-channel"
 bucket="s3://$bucket/linux-64"
 
 pkg="yhat==1.9.0"
 pkgname=$(python -c "print '$pkg'.split('==')[0]")
 pkgversion=$(python -c "print '$pkg'.split('==')[1]")
 echo "building $pkg $pkgname $pkgversion"
-set -ex
+
 mkdir /channel
 cd /channel
 aws s3 sync $bucket/.index.json .

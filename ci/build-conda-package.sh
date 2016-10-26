94eba38a8a791fa15b9e60fb3649c81655110685
Greg Lamp <lamp.greg@gmail.com>
2016-10-26 15:52:22 -0400

    cross compile

--- ci/build-conda-package.sh
+++ ci/build-conda-package.sh
@@ -3,26 +3,24 @@
 set -ex
 
 bucket="yhat-conda-channel"
-mkdir "${bucket}"
-cd "${bucket}"
+mkdir "~/${bucket}"
+cd "~/${bucket}"
 
 bucket="s3://$bucket/linux-64"
+aws s3 sync $bucket .
 
 pkgname="yhat"
 pkgversion=$(python -c "from yhat import version; print(version.__version__)")
-echo "building $pkg $pkgname $pkgversion"
+echo "building ${pkgname} ${pkgversion}"
 
-aws s3 sync $bucket/.index.json .
-aws s3 sync $bucket/repodata.json .
-aws s3 sync $bucket/repodata.json.bz2 .
-pip install $pkg
+# build conda for default OS and then cross-compile for other OS
+mkdir ~/conda-builds/
 conda package --pkg-name "$pkgname" --pkg-version "$pkgversion"
+conda convert --platform all "./yhat-${pkgversion}-py27_0.tar.bz2" -o ~/conda-builds/
+cd "~/${bucket}"
+sudo cp -R ~/conda-builds/* ./
 conda index --no-remove
 tree .
-
-mkdir ~/conda-builds/
-conda convert --platform all "./yhat-${pkgversion}-py27_0.tar.bz2" -o ~/conda-builds/
-tree ~/conda-builds/
 
 
 if [[ "$1" == "upload" ]]; then

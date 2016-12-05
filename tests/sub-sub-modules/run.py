148594bd40f4f5f2a7d603660c4b6ef3e32e6425
Greg Lamp <lamp.greg@gmail.com>
2016-12-05 12:31:21 -0800

    removing key

--- tests/sub-sub-modules/run.py
+++ tests/sub-sub-modules/run.py
@@ -2,11 +2,10 @@ from example_app.models.yhat_model import TestModel, Foo
 from yhat import Yhat
 import json
 
-yh = Yhat("greg", "7e00601199a406babda135e3e05dd2d6", "http://do-sb-kernel-master.x.yhat.com")
 yh = Yhat(
         "greg",
-        "d8c90dfecd2dc088a297c3cb21de1770",
-        "https://sandbox.c.yhat.com"
+        "foo",
+        "http://api.yhat.com/"
         )
 TestModel().execute(1)
 
